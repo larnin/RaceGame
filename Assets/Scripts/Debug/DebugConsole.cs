da6fb2ce44f5370c32c856770e14f561ae8692e2
Nico <nicolas-donald.laurent@laposte.net>
2022-11-15 00:00:54 +0100

    Thread Jobs

---
+++ Assets/Scripts/Debug/DebugConsole.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class DebugConsole : MonoBehaviour
+{
+    Color[] m_logColors = new Color[]
+    {
+        Color.white,
+        Color.yellow,
+        Color.red
+    };
+
+    const float m_spacing = 15;
+    const int m_maxLines = 50;
+    static readonly object m_linesLock = new object();
+    static List<Line> m_lines = new List<Line>();
+
+    static int m_mainThreadID;
+
+    enum LogType
+    {
+        Log,
+        Warning,
+        Error
+    }
+
+    class Line
+    {
+        public Line(string _text, LogType _logType)
+        {
+            text = _text;
+            logType = _logType;
+            sent = false;
+        }
+
+        public string text;
+        public LogType logType;
+        public bool sent;
+    }
+
+#if !UNITY_EDITOR
+    private void OnGUI()
+    {
+        lock (m_linesLock)
+        {
+            for (int i = 0; i < m_lines.Count; i++)
+            {
+                GUI.contentColor = m_logColors[(int)(m_lines[i].logType)];
+                GUI.Label(new Rect(10, 10 + m_spacing * i, 500, 50), m_lines[i].text);
+            }
+        }
+    }
+#endif
+
+    private void Start()
+    {
+        m_mainThreadID = System.Threading.Thread.CurrentThread.ManagedThreadId;
+    }
+
+    private void Update()
+    {
+        lock (m_linesLock)
+        {
+            foreach(var l in m_lines)
+            {
+                if(!l.sent)
+                {
+                    l.sent = true;
+                    LogLine(l.text, l.logType);
+                }
+            }
+        }
+    }
+
+    public static void Log(string line)
+    {
+        AddLine(line, LogType.Log);
+    }
+
+    public static void Warning(string line)
+    {
+        AddLine(line, LogType.Warning);
+    }
+
+    public static void Error(string line)
+    {
+        AddLine(line, LogType.Error);
+    }
+
+    static void AddLine(string line, LogType type)
+    {
+        Line l = new Line(line, type);
+        if(System.Threading.Thread.CurrentThread.ManagedThreadId == m_mainThreadID)
+        {
+            LogLine(l.text, l.logType);
+            l.sent = true;
+        }
+        lock (m_linesLock)
+        {
+            m_lines.Add(l);
+            while (m_lines.Count > m_maxLines)
+                m_lines.RemoveAt(0);
+        }
+    }
+
+    //must be on the main thread to do it
+    static void LogLine(string line, LogType type)
+    {
+        if (type == LogType.Log)
+            Debug.Log(line);
+        else if (type == LogType.Warning)
+            Debug.LogWarning(line);
+        else if (type == LogType.Error)
+            Debug.LogError(line);
+    }
+}
---
+++ Assets/Scripts/ThreadPool.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static class ThreadPool
+{
+    class Job
+    {
+        public Action job;
+        public Action endCallback;
+        public int priority;
+
+        public Job(Action _job, Action _endCallback, int _priority = 1)
+        {
+            job = _job;
+            endCallback = _endCallback;
+            priority = _priority;
+        }
+    }
+
+    const int m_threadCount = 2;
+
+    static bool m_started = false;
+    static bool m_aborted = false;
+    static Thread[] m_threads = new Thread[m_threadCount];
+
+    static readonly object m_pendingJobsLock = new object();
+    static List<Job> m_pendingJobs = new List<Job>();
+
+    static bool WantsToQuit()
+    {
+        if (!m_started)
+            return true;
+
+        Debug.Log("Stopping thread pool");
+
+        m_aborted = true;
+        for(int i = 0; i < m_threadCount; i++)
+        {
+            if (m_threads[i] != null)
+                m_threads[i].Join();
+        }
+
+        return true;
+    }
+
+    [RuntimeInitializeOnLoadMethod]
+    static void RunOnStart()
+    {
+        Application.wantsToQuit += WantsToQuit;
+    }
+
+    static void InitThreads()
+    {
+        m_started = true;
+        for(int i = 0; i < m_threadCount; i++)
+        {
+            m_threads[i] = new Thread(new ThreadStart(Process));
+            m_threads[i].Start();
+        }
+    }
+
+    static void Process()
+    {
+        while(!m_aborted)
+        {
+            Job j = null;
+            lock(m_pendingJobsLock)
+            {
+                if (m_pendingJobs.Count() != 0)
+                {
+                    j = m_pendingJobs[0];
+                    m_pendingJobs.RemoveAt(0);
+                }
+            }
+            if (j == null)
+                Thread.Sleep(10);
+            else DoJob(j);
+        }
+    }
+
+    static void DoJob(Job j)
+    {
+        j.job();
+        j.endCallback();
+    }
+
+    static void StartJob(Action job, Action endCallback, int priority = 1)
+    {
+        if (!m_started)
+            InitThreads();
+
+        if (m_aborted)
+            return;
+
+        Job j = new Job(job, endCallback, priority);
+
+        lock(m_pendingJobsLock)
+        {
+            if (m_pendingJobs.Count == 0 || m_pendingJobs[m_pendingJobs.Count - 1].priority >= priority)
+                m_pendingJobs.Add(j);
+            else
+            {
+                for (int i = 0; i < m_pendingJobs.Count; i++)
+                {
+                    if (priority > m_pendingJobs[i].priority)
+                    {
+                        m_pendingJobs.Insert(i, j);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
